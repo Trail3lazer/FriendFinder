ebb56ab27f36a6194f8ab9978e9b0e4926f592d1
jaredrwight <20687190+Trail3lazer@users.noreply.github.com>
2019-08-09 13:42:41 -0600

    added logic for finding a best friend

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,7 +2,48 @@ var friends = [
     {
         name: "Cookie Monster",
         age: "30",
-        picture:"https://cdn.shopify.com/s/files/1/0043/8471/8938/products/154360576926714891_1049x.jpg?v=1555127662"
+        picture:"https://cdn.shopify.com/s/files/1/0043/8471/8938/products/154360576926714891_1049x.jpg?v=1555127662",
+        stats: [5,3,4,2,5,5,1,2,3,4,5,2,3,5]
+    },{
+        name: "Wendy",
+        age: "50",
+        picture:"https://i.pinimg.com/originals/3d/d6/11/3dd61173b7f1796846d12685a07afa3c.jpg",
+        stats: [5,3,6,5,4,2,5,3,2,1,5,2,2,4]
+    },{
+        name: "Daryl",
+        age: "31",
+        picture:"https://i.pinimg.com/originals/d8/6a/5d/d86a5d727f64dc53f829724ac8c6dca4.jpg",
+        stats: [1,3,4,5,3,2,1,4,2,4,5,1,3,5]
+    },{
+        name: "Michael",
+        age: "40",
+        picture:'http://cdn3.whatculture.com/images/2018/11/e46c3d8de0c01966-600x338.jpg',
+        stats: [5,1,5,1,1,5,5,1,5,5,1,1,1,5]
+    },{
+        name: "Dwight",
+        age: "34",
+        picture:'https://i.imgur.com/5wnc1T9.jpg',
+        stats: [1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+    },{
+        name: "Dwigt",
+        age: "<a href='https://www.youtube.com/watch?v=-FoKU54ITuI'>Video</a>",
+        picture:'https://i.redd.it/wylt103jueh11.jpg',
+        stats: [5,5,5,5,5,5,5,5,5,5,5,5,5,5]
+    },{
+        name: "Stanley",
+        age: "45",
+        picture:'https://wsbt.com/resources/media/a635a1b6-4cc4-425b-b686-67fa860f9ed8-large16x9_stanleyhudsonNBC.PNG?1550090897080',
+        stats: [3,2,4,5,1,3,5,1,2,4,2,5,1,2]
+    },{
+        name: "Pam",
+        age: "34",
+        picture:'https://imgix.bustle.com/uploads/image/2019/5/28/44774e68-1522-49ec-87c8-6c966a499b8c-pamtheoffice.png?w=1020&h=574&fit=crop&crop=faces&auto=format&q=70',
+        stats: [3,3,3,3,3,3,3,3,3,3,3,3,3,3]
+    },{
+        name: "Jim",
+        age: "34",
+        picture:'https://d13ezvd6yrslxm.cloudfront.net/wp/wp-content/images/tomclancy-jimryan-mashup-700x362.jpg',
+        stats: [2,2,2,2,2,2,2,2,2,2,2,2,2,2]
     }
 ]
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,37 @@
 module.exports = (app, path) =>{
-    var friends = require('../data/friends')
-    app.get('/api/friends', (req, res) => {
+
+var friends = require('../data/friends')
+
+app.get('/api/friends', (req, res) => {
     res.json(friends)
 })
 
 app.post('/api/friends', (req, res) => {
     var newFriend = req.body;
-    console.log(newFriend);
+    let bestFriend = {
+        dif = 100,
+        obj
+    };
+    for (i in friends){
+        let dude = friends[i];
+        let dudeTotal = 0;
+        let friendTotal = 0;
+        let dif;
+        for (idx in dude.stats){
+            dudeTotal += dude.stats[idx];
+            friendTotal += friend.stats[idx];
+        };
+        if(dudeTotal===friendTotal){
+            bestFriend.dif = 0;
+            bestFriend.obj = dude}
+        else if (dudeTotal>friendTotal){dif = dudeTotal-friendTotal}
+        else {dif = friendTotal-dudeTotal};
+        if(dif<bestFriend.dif){
+            bestFriend.dif = dif;
+            bestFriend.obj = dude};
+    };
+    bestFriend = bestFriend.obj;
     friends.push(newFriend);
-    res.json(newFriend);
+    res.json(bestFriend);
 })
 };
